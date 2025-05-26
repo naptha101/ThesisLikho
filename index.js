9ed69a927d7061002790bf9e39e14a80146f5347
Yash Verma <yash@stuvalley.com>
2025-05-26 18:05:49 +0530

    new commit

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ import cookieParser from 'cookie-parser'
 
 
 const app=express()
-app.use(cors());
+app.use(cors({
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+app.use(express.urlencoded({ extended: true })); // add this
 
 app.use(cookieParser())
 dotenv.config()
